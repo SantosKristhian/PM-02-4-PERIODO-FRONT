adfbc5f5cba9003be97863d81e422af636f82b1c
SantosKristhian <gomess2012@live.com>
2025-09-20 10:06:25 -0300

    adicionando mdb-angular kit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,11 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient() // 👈 ESSENCIAL para habilitar HttpClient globalmente
+    provideHttpClient(), provideAnimations() // 👈 ESSENCIAL para habilitar HttpClient globalmente
   ]
 };
